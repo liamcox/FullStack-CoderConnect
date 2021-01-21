e94ba8108b04ba9863185b2867f6e2d82ae530e5
Liam Cox <liamcox@Liams-MacBook-Pro.local>
2021-01-21 10:35:59 -0600

    production prep

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 //connect database
 connectDB();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res) => res.send("api running"));
 
 // Init Middleware
 app.use(express.json({ extended: false }));
@@ -17,5 +15,19 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/profile", require("./routes/api/profile"));
+
+//Server static assets in production
+if (process.env.NODE_ENV === "production") {
+    //Set static folder
+    app.use(express.static("frontend/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, "frontend", "build", "index.html"),
+        );
+    });
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started on ${PORT}`));
